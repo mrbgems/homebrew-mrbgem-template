2fe3912ed7eec74dd9f4c8147f6b1a0a12e53a9c
Kondo Uchio <udzura@udzura.jp>
2020-10-19 14:26:08 +0900

    Bump versuon to release 0.3

--- Formula/mrbgem-template.rb
+++ Formula/mrbgem-template.rb
@@ -6,23 +6,13 @@ class MrbgemTemplate < Formula
   homepage "https://github.com/matsumotory/mruby-mrbgem-template"
   head "https://github.com/matsumotory/mruby-mrbgem-template.git"
   # TODO point to origin
-  url "https://github.com/matsumotory/mruby-mrbgem-template/archive/v0.2.1.tar.gz"
-  sha256 "b7ea5d26bfad88cea54e6955f556875e2c8405f497bca80bf6e519277f4eeb8d"
+  url "https://github.com/matsumotory/mruby-mrbgem-template/archive/v0.3.0.tar.gz"
+  sha256 "f8b9ba25b953311f24babd9b3ed74d3ba95dd241f0c32629e4c723bb935ebf36"
   # depends_on "cmake" => :build
 
   def install
     system "rake"
     bin.install "mruby/bin/mrbgem-template"
-  end
-  
-  patch do
-    url "https://github.com/matsumotory/mruby-mrbgem-template/commit/d86552189456c11d2fd09eed8d2be502fb5497a5.patch"
-    sha256 "b2bdd10b1b4ffeff59d06ee1033fb1d82aab9404854bb1a56afff72376b4f11c"
-  end
-  
-  patch do
-    url "https://github.com/matsumotory/mruby-mrbgem-template/commit/ed259f30a7c2fac2d15b136b8a2a974f5b8d4fa0.patch"
-    sha256 "fab4854ae22d25c90555fa20aa9f4274d354e8760b1791ab144b3ed4ecac1be2"
   end
 
   test do
